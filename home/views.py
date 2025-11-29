bb3cc01d6abb019b2c8641d4fb23055aeeab3e6c
pauloifpi <paulorodrigo1098@gmail.com>
2025-11-28 22:22:07 -0300

    vercel

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("A view index funcionou, Wow!")   
+
+def sobre(request):
+    return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim mesmo.</h1>")  
+
+def contato(request):
+    return HttpResponse("Esta é a página de contato.")
+
+def ajuda(request):
+    return HttpResponse("Esta é a página de ajuda.")
