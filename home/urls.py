d745b582873b9c70f118241fde384adb92ebb6d9
pauloifpi <paulorodrigo1098@gmail.com>
2025-12-04 23:15:36 -0300

    Vercel Atualizado

--- home/urls.py
+++ home/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
     path('ajuda/', views.ajuda, name='ajuda'),
+    path('item/<int:id>/', views.exibir_item, name='exibir_item'),
+    path('perfil/<str:usuario>/', views.perfil, name='perfil'),
+    path('diasemana/<int:num>/', views.diasemana, name='dia'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("A view index funcionou, Wow!")   
+    return render(request, 'index.html')
 
 def sobre(request):
-    return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim mesmo.</h1>")  
+    return render(request, 'sobre.html') 
 
 def contato(request):
-    return HttpResponse("Esta é a página de contato.")
+    return render(request, 'contato.html')
 
 def ajuda(request):
-    return HttpResponse("Esta é a página de ajuda.")
+    return render(request, 'ajuda.html')
+
+def exibir_item(request, id):
+    return render(request, 'exibir_item.html', {'id': id})
+
+def perfil(request, usuario):
+    return render(request, 'perfil.html', {'usuario': usuario})
+
+def diasemana(request, num):
+
+    dias = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado",
+    }
+
+    dia = dias.get(num, "Dia inválido!")
+
+    return render(request, 'dia.html', {'dia': dia})
